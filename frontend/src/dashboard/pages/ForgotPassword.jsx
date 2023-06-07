aa9d8cf4ef9d5729258654a0154be9f9cc272ed6
Ritik Bhateley <ritikbhateley125@gmail.com>
2023-06-07 13:07:12 +0530

    Forgot Password Page added

--- frontend/src/dashboard/pages/ForgotPassword.jsx
+++ frontend/src/dashboard/pages/ForgotPassword.jsx
@@ -7,27 +7,27 @@ const ForgotPassword = () => {
         <div>
             <div className="flex flex-col place-content-center place-items-center">
            
-           <nav className="flex place-content-start place-items-center w-2/3 h-20 border-b-2">
-             <Link to="/" className="ml-20">
-               <div className="text-3xl font-black  text-center text-indigo-600">
-                 SHORTENITE
-               </div>
-             </Link>
-           </nav>
- 
-           <div className="flex flex-col  w-1/3 mx-16 mt-24 rounded-lg shadow-lg ">
-             <span className="text-3xl font-semibold text-center">Forgot Password</span>
- 
-             <div className="mx-16 mt-20">
-                 <label className="text-lg font-normal" for="name">Username or Email Address</label>
-                 <Input className="mt-3" variant="outline" placeholder="" />
-             </div>
+            <nav className="flex place-content-start place-items-center w-2/3 h-20 border-b-2">
+                <Link to="/" className="ml-20">
+                <div className="text-3xl font-black  text-center text-indigo-600">
+                    SHORTENITE
+                </div>
+                </Link>
+            </nav>
+    
+            <div className="flex flex-col  w-1/3 mx-16 mt-24 rounded-lg shadow-lg ">
+                <span className="text-3xl font-semibold text-center">Forgot Password</span>
+    
+                <div className="mx-16 mt-20">
+                    <label className="text-lg font-normal" for="name">Username or Email Address</label>
+                    <Input className="mt-3" variant="outline" placeholder="" />
+                </div>
 
-             <Button className="my-12 mx-16" colorScheme='blue'>Reset</Button>
- 
-           </div>
- 
-       </div>
+                <Button className="my-12 mx-16" colorScheme='blue'>Reset</Button>
+    
+            </div>
+    
+        </div>
         </div>
     )
 }
