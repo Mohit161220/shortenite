1edf3fb1e3dd469e217b04d172f11cad937af9ae
iamprincesuman <iamprincesuman@gmail.com>
2023-06-08 14:53:25 +0530

    Commiting

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const linksController = require('../controllers/links_controller');
 
-console.log('Routes -> Index.js');
-
 router.use('/users', require('./users'));
 router.use('/links', require('./links'));
 router.get('/:id', linksController.handleRedirect);
--- backend/routes/links.js
+++ backend/routes/links.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const passport = require('passport');
 const linksController = require('../controllers/links_controller');
 
+// limk status route and controller to be added..
+
 router.get('/', passport.checkAuthentication, linksController.getAllLinksofUser);
 router.post('/create-link', passport.checkAuthentication , linksController.createLink);
 router.patch('/edit/:id', passport.checkAuthentication, linksController.edit);
