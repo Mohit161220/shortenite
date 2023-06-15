c8f7d3865bcb2448695a677f79d0bc6e751e1393
Mohit Singh Rana <mohitsinghrana_co20b5_18@dtu.ac.in>
2023-06-15 10:49:24 +0530

    Image download added

--- frontend/src/dashboard/components/QrList.jsx
+++ frontend/src/dashboard/components/QrList.jsx
@@ -5,9 +5,9 @@ import ViewDetailsButton from "./ViewDetailsButton";
 import { saveAs } from "file-saver";
 
 const QrList = () => {
-  const downloadImage = (img,title) => {
+  const downloadImage =(img,title) => {
     console.log("Downloading");
-    // saveAs(img, `${title}.jpg`); // Put your image url here.
+    saveAs(img, `${title}.jpg`); // Put your image url here.
   };
 
   const [data, setData] = useState([]);
@@ -31,7 +31,7 @@ const QrList = () => {
 
             <div className="flex">
               <img src={l.key} alt="QR" className=" my-1 mx-4 w-24 h-24"></img>
-              <button onClick={downloadImage(l.key,l.title)}>
+              <button onClick={()=>downloadImage(l.key,l.title)}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -46,7 +46,6 @@ const QrList = () => {
                     d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
                   />
                 </svg>
-                Download
               </button>
               {/* 
               <span className="flex ml-6 mt-8">
